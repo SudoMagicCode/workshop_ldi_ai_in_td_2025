7a0f31e6846a9f18ac3a1341657633543ee2e09b
Ian Shelanskey <ishelanskey@gmail.com>
2025-12-03 09:46:13 -0800

    adds more endpoints for examples, adds docker for easy build and deploy

--- code/api-example-server/cmd/server/main.go
+++ code/api-example-server/cmd/server/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
+
+var messages = map[string]string{}
 
 func main() {
 	// Create a Gin router with default middleware (logger and recovery)
@@ -21,12 +24,29 @@ func main() {
 		})
 	})
 
-	r.PUT("/hello", func(c *gin.Context) {
+	r.GET("/hello/:id", func(c *gin.Context) {
 
-		buf := []byte{}
-		c.Request.Body.Read(buf)
+		if name, ok := messages[c.Param("id")]; ok {
+			// Return JSON response
+			c.JSON(http.StatusOK, gin.H{
+				"message": fmt.Sprintf("Hello, %s", name),
+			})
+			return
+		}
+		// Return JSON response
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "id is not ready yet...",
+		})
+		return
+	})
+
+	r.POST("/hello", func(c *gin.Context) {
+
 		msg := map[string]string{}
-		json.Unmarshal(buf, &msg)
+		if err := c.ShouldBindBodyWithJSON(&msg); err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
 
 		if name, ok := msg["name"]; ok {
 
@@ -36,6 +56,37 @@ func main() {
 			})
 			return
 		}
+
+		// Return JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, there",
+		})
+	})
+
+	r.POST("/delayed-hello", func(c *gin.Context) {
+		id := uuid.NewString()
+
+		msg := map[string]string{}
+		if err := c.ShouldBindBodyWithJSON(&msg); err != nil {
+			c.AbortWithError(500, err)
+			return
+		}
+
+		if name, ok := msg["name"]; ok {
+
+			// make a go routine that delayes hello being ready
+			go func() {
+				<-time.After(10 * time.Second)
+				messages[id] = name
+			}()
+
+			// Return JSON response
+			c.JSON(http.StatusOK, gin.H{
+				"id": id,
+			})
+			return
+		}
+
 		// Return JSON response
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, there",
